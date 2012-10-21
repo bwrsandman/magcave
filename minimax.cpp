645bbbf3496b62deb650196e5c684f519370e28a
Sandy Carter <bwrsandman@gmail.com>
2012-10-21 13:18:49 -0400

    alpha beta

--- minimax.cpp
+++ minimax.cpp
@@ -15,29 +15,34 @@ int main(int argc, char * argv[])
 }
 
 /* Minimax function. */
-// TODO: Alpha beta pruning
-const signed char minimax(const minimaxNode* node, const char depth, bool max)
+const signed char minimax(const minimaxNode* node, const char depth, signed char alpha, signed char beta, bool max)
 {
-    /* Set alpha to -infinity, in this case, minimum CHAR value */
-    signed char alpha = (max) ? SCHAR_MIN : SCHAR_MAX;
     /* Base Case, leaf node. */
     if (!node->children or !depth)
     {
         return heur(node);
     }
     /* Recursive Step, using max everytime, but doing *-1 at each step. */
-    for (minimaxNode *child = node->children; child != NULL; child = child->next)
+    if (max)
     {
-        if (max)
+        for (minimaxNode *child = node->children; child != NULL; child = child->next)
         {
-            alpha = std::max(alpha, (signed char)(minimax(child, depth-1, !max)));
+            alpha = std::max(alpha, (signed char)(minimax(child, depth-1, alpha, beta, !max)));
+            if (beta <= alpha)
+                break;
         }
-        else
-        {
-            alpha = std::min(alpha, (signed char)(minimax(child, depth-1, !max)));
-        }
+        return alpha;
     }
-    return alpha;
+    else
+    {
+        for (minimaxNode *child = node->children; child != NULL; child = child->next)
+        {
+            beta = std::min(beta, (signed char)(minimax(child, depth-1, alpha, beta, !max)));
+            if (alpha <= beta)
+                break;
+        }
+        return beta;
+    }
 }
 
 /* Heuristic function */
--- minimax.h
+++ minimax.h
@@ -8,7 +8,12 @@ struct minimaxNode
 };
 
 const signed char heur(const minimaxNode* n);
-const signed char minimax(const minimaxNode* n, const char depth, bool max=true);
+/* Set alpha to -infinity and beta to +infinity, in this case, 
+ * minimum and maximum CHAR values */
+const signed char minimax(const minimaxNode* n, const char depth, 
+                          signed char alpha=SCHAR_MIN, 
+                          signed char beta=SCHAR_MAX, 
+                          bool max=true);
 const char * const xmlfilename = "tree.xml";
 const minimaxNode* readxmltree(const char* const filename);
 const minimaxNode* readxmlnode(const xmlNode* const root,
